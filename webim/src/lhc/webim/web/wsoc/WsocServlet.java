fe498f5964139f5f794b03e1687ac65d5ade7975
redstarofsleep <redstarofsleep@163.com>
2013-02-21 17:02:36 +0800

    first

---
+++ webim/src/lhc/webim/web/wsoc/WsocServlet.java
@@ -0,0 +1,17 @@
+package lhc.webim.web.wsoc;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.catalina.websocket.StreamInbound;
+import org.apache.catalina.websocket.WebSocketServlet;
+
+public class WsocServlet extends WebSocketServlet {
+
+	@Override
+	protected StreamInbound createWebSocketInbound(String arg0,
+			HttpServletRequest arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
